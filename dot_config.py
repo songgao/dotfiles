2e6f3c56ae8d1f231e53b715388e3e5cedd27fdd
Song Gao <song@gao.io>
2014-01-25 15:54:56 -0600

    added oh-my-zsh

--- dot_config.py
+++ dot_config.py
@@ -3,7 +3,20 @@ import os
 HOME = os.getenv("HOME")
 
 confs = {
-        'tmux': [('f', '.tmux.conf', HOME + '/.tmux.conf'), ('d', '.tmux', HOME + '/.tmux'), ],
-        'vim': [('f', '.vimrc', HOME + '/.vimrc'), ('d', '.vim', HOME + '/.vim'), ],
-        'git': [('f', '.gitconfig', HOME + '/.gitconfig'), ('f', '.gitignore_global', HOME + '/.gitignore_global')],
+        'tmux': [
+            ('f', '.tmux.conf', HOME + '/.tmux.conf'),
+            ('d', '.tmux', HOME + '/.tmux'),
+            ],
+        'vim': [
+            ('f', '.vimrc', HOME + '/.vimrc'),
+            ('d', '.vim', HOME + '/.vim'),
+            ],
+        'git': [
+            ('f', '.gitconfig', HOME + '/.gitconfig'),
+            ('f', '.gitignore_global', HOME + '/.gitignore_global')
+            ],
+        'zsh': [
+            ('f', '.zshrc', HOME + '/.zshrc'),
+            ('d', '.oh-my-zsh', HOME + '/.oh-my-zsh'),
+            ],
         }
