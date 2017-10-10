44a4c94a673df4d1ed309bf24bac72233cc762cd
Song Gao <song@gao.io>
2017-10-10 11:25:37 -0700

    add fish

---
+++ confs/.config/fish/config.fish
@@ -0,0 +1,39 @@
+
+set -x PATH $HOME/.local/bin $HOME/bin /usr/local/bin /usr/local/sbin $PATH
+
+if [ (uname) = 'Darwin' ]
+    # GNU coreutils
+    set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+    set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+end
+
+alias less="less -R"
+alias grep="grep --color=always"
+alias p8="ping 8.8.8.8"
+
+# Go
+set -x GOROOT $HOME/go
+set -x GOPATH $HOME/gopath
+set -x GOBIN $GOPATH/bin
+set -x PATH $GOROOT/bin $GOBIN $PATH
+alias gf="gofmt -s -w -e -l"
+if which colorgo >/dev/null ^/dev/null
+    alias go="colorgo"
+end
+
+# Editors
+set -x VISUAL "nvim"
+set -x EDITOR "$VISUAL"
+
+# powerline prompt
+if which powerline-go >/dev/null ^/dev/null
+    function fish_prompt
+        powerline-go -error $status -shell bare
+    end
+end
+
+# source ~/.rc.fish.local  
+if test -e ~/.rc.fish.local
+    source ~/.rc.fish.local
+end
+
