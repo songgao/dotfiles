dd112dc22879e9c48079fdcb77e3317f961f6864
Song Gao <song.gao@zoom.us>
2023-10-15 23:06:43 -0700

    color

---
+++ confs/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "sainnhe/sonokai" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "sonokai",
+    },
+  },
+}
